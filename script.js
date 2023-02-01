d522dcb3544dcf8cdc0610ed380dee1700c7b0d4
NelleStar <nellestar@TABLET-3FCFK5MC>
2023-02-01 11:11:34 -0500

    dont know what I did to json, but updated html, js and css

---
+++ script.js
@@ -0,0 +1,15 @@
+// create random RGB
+function randomRBG() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+}
+
+// create a way to change each letter independantly
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function() {
+    for (let letter of letters) {
+        letter.style.color = randomRBG
+    }
+}, 1000);
